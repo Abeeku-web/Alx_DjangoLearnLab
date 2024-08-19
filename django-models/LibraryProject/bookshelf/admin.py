5c50f9c7bc2daec3271ab28f96e4a0fc640ecaf7
abeekuhag <abee.hagan@gmail.com>
2024-08-19 00:36:25 +0000

    deleted Book registration in admin

--- django-models/LibraryProject/bookshelf/admin.py
+++ django-models/LibraryProject/bookshelf/admin.py
@@ -1,12 +1,1 @@
 from django.contrib import admin
-from .models import Book
-
-class BookAdmin(admin.ModelAdmin):
-    #Customize the list display
-    list_display = ('title','author','publication_year')
-
-    #Add search fields
-    search_fields = ('title','author')
-
-    #Add list filters
-    list_filter = ('publication_year',)
