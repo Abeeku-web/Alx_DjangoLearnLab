2c1194d7fa0963e4c81e9ae3bdd4cc876e5eebde
abeekuhag <abee.hagan@gmail.com>
2024-08-25 22:16:16 +0000

    .

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -18,10 +18,14 @@ from django.contrib import admin
 from django.urls import path
 from .views import list_books, LibraryDetailView
 from django.urls import include, path
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('books/', list_books, name='list_books'),
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
     path('relationship_app/', include('relationship_app.urls')),
+    path('login/', auth_views.LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+    path('register/', views.register, name='register'),
 ]
