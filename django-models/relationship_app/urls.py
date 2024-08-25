6d5b98ddb066cac078dbcc35f22fb0fe44e1af9b
abeekuhag <abee.hagan@gmail.com>
2024-08-25 22:37:07 +0000

    Added views

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -19,6 +19,7 @@ from django.urls import path
 from .views import list_books, LibraryDetailView
 from django.urls import include, path
 from django.contrib.auth import views as auth_views
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,4 +29,7 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
     path('register/', views.register, name='register'),
+    path('admin-view/', views.admin_view, name='admin_view'),
+    path('librarian-view/', views.librarian_view, name='librarian_view'),
+    path('member-view/', views.member_view, name='member_view'),
 ]
