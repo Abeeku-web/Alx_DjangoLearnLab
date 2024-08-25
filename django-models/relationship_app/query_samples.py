b5734e231f023d0eb0f6905628a6b5d5f67572b1
abeekuhag <abee.hagan@gmail.com>
2024-08-25 19:59:14 +0000

    Edited code to include "objects.filter(author=author)"

--- django-models/relationship_app/query_samples.py
+++ django-models/relationship_app/query_samples.py
@@ -2,15 +2,22 @@ from relationship_app.models import Author, Book, Library, Librarian
 
 # Query all books by a specific author
 def get_books_by_author(author_name):
+    # Retrieve the author object
     author = Author.objects.get(name=author_name)
-    return author.books.all()
+    # Use objects.filter() to get all books by the author
+    books = Book.objects.filter(author=author)
+    return books
 
 # List all books in a specific library
 def get_books_in_library(library_name):
+    # Retrieve the library object
     library = Library.objects.get(name=library_name)
+    # Use the related name 'books' to get all books in the library
     return library.books.all()
 
 # Retrieve the librarian for a specific library
 def get_librarian_for_library(library_name):
+    # Retrieve the library object
     library = Library.objects.get(name=library_name)
+    # Use the related name 'librarian' to get the librarian for the library
     return library.librarian
