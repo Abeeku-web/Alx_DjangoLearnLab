5265e0991780796631f2750f40e906d94f3ddad6
abeekuhag <abee.hagan@gmail.com>
2024-09-15 01:20:58 +0000

    Configured Authentication

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path, include
+from rest_framework.authtoken.views import obtain_auth_token
 from rest_framework.routers import DefaultRouter
 from .views import BookList, BookViewSet
 
@@ -8,5 +9,6 @@ router.register(r'books_all', BookViewSet, basename='book_all')
 
 urlpatterns = [
     path('books/', BookList.as_view(), name= 'book-list'),
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'), # Token retrieval endpoint
     path('', include(router.urls)), # This includes all routes registered with the router
 ]
