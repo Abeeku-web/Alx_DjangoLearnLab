066794783464c0b3879cde412e81f511500c4bfc
abeekuhag <abee.hagan@gmail.com>
2024-09-15 04:58:38 +0000

    Configured url patterns Implemented profile management

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.contrib.auth import views as auth_views
+from django.urls import reverse_lazy
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from .forms import CustomUserCreationForm
 
-# Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'blog/register.html', {'form': form})
+
+@login_required
+def profile(request):
+    return render(request, 'blog/profile.html')
