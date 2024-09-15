fc90030b1919cbb475eb98d26c930c05bcd05408
abeekuhag <abee.hagan@gmail.com>
2024-09-15 23:57:41 +0000

    .

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from .models import Post
+from .models import Comment
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
@@ -22,3 +23,12 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['title', 'content']  # You can include other fields if necessary
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['content']  # Include the fields you want users to fill out
+        widgets = {
+            'content': forms.Textarea(attrs={'rows': 3, 'placeholder': 'Enter your comment...'}),
+        }
