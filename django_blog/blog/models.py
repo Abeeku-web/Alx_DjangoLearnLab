32856db03b64584c0bcabbe6f37ad5d0d3c5e72f
abeekuhag <abee.hagan@gmail.com>
2024-09-15 22:57:00 +0000

    Tried fixing circular import

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,7 +1,23 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
+from .models import Post
 
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Comment by {self.author} on {self.post}"
+
+    class Meta:
+        ordering = ['-created_at']  # Newest comments first
+
+        
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
@@ -21,15 +37,3 @@ class UserProfile(models.Model):
         return self.user.username
 
 
-class Comment(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-    content = models.TextField()
-    created_at = models.DateTimeField(default=timezone.now)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return f"Comment by {self.author} on {self.post}"
-
-    class Meta:
-        ordering = ['-created_at']  # Newest comments first
