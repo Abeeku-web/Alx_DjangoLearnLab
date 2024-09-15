1144b16745f387486a78464781738e30a2ea2d5c
abeekuhag <abee.hagan@gmail.com>
2024-09-15 22:43:02 +0000

    Created Comment model

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
+from .models import Post
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
@@ -18,3 +20,17 @@ class UserProfile(models.Model):
 
     def __str__(self):
         return self.user.username
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Comment by {self.author} on {self.post}"
+
+    class Meta:
+        ordering = ['-created_at']  # Newest comments first
