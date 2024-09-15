4e44057683d474ddb61db4deab48b7dbe56ec182
abeekuhag <abee.hagan@gmail.com>
2024-09-15 04:26:44 +0000

    .

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -121,9 +121,17 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 
 TEMPLATES = [
     {
-        # ...
-        'DIRS': [BASE_DIR / 'templates'],
-        # ...
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [BASE_DIR / "templates"],  # Add your custom template directory here if needed
+        'APP_DIRS': True,  # This allows Django to find templates inside apps
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
     },
 ]
 
