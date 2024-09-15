a1542acd4a0a9306bbb0ce608963d9037c1d8a3b
abeekuhag <abee.hagan@gmail.com>
2024-09-15 22:54:33 +0000

    Tried fixing circular import

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
-from .models import Post
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
