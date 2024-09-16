09c1e542689a330bc1f2a05f3d12c77d4255ac10
abeekuhag <abee.hagan@gmail.com>
2024-09-16 00:10:04 +0000

    .

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -2,12 +2,18 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
 
+class Tag(models.Model):
+    name = models.CharField(max_length=100, unique=True)
 
+    def __str__(self):
+        return self.name
+    
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     published_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    tags = models.ManyToManyField(Tag, related_name='posts')
 
     def __str__(self):
         return self.title
@@ -34,3 +40,4 @@ class Comment(models.Model):
 
     class Meta:
         ordering = ['-created_at']  # Newest comments first
+
