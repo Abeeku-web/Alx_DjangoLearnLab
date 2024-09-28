998977baa7ac26f263d54b96a462958744c438bb
abeekuhag <abee.hagan@gmail.com>
2024-09-28 21:14:10 +0000

    Changed database to PostgreSQL

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
+        'NAME': 'PostgreSQL',
         'USER': 'lenovo',
         'PASSWORD': 'Workwell7',
         'HOST': 'localhost',
