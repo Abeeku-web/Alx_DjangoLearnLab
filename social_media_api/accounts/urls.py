f8d8ffae44aedbb4d3f802d1eddf569b87ad4a83
abeekuhag <abee.hagan@gmail.com>
2024-09-29 23:48:30 +0000

    Added follows and feeds functionality

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import RegisterView, CustomAuthToken, ProfileView, accounts_home, accounts_redirect
+from . import views
 
 urlpatterns = [
     path('', accounts_home, name='accounts_home'),  # Handle /api/accounts/ path
@@ -7,4 +8,6 @@ urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', CustomAuthToken.as_view(), name='login'),
     path('profile/', ProfileView.as_view(), name='profile'),
+    path('follow/<int:user_id>/', views.follow_user, name='follow_user'),
+    path('unfollow/<int:user_id>/', views.unfollow_user, name='unfollow_user'),
 ]
--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, generics
+from rest_framework.permissions import IsAuthenticated
 from .models import Post, Comment
 from .serializers import PostSerializer, CommentSerializer
 from django_filters.rest_framework import DjangoFilterBackend
+
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all().order_by('-created_at')
@@ -22,3 +24,15 @@ class CommentViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
+
+
+class FeedView(generics.ListAPIView):
+    serializer_class = PostSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        # Get the current user's following list
+        user = self.request.user
+        following_users = user.following.all()
+        # Return posts authored by followed users, ordered by creation date
+        return Post.objects.filter(author__in=following_users).order_by('-created_at')
