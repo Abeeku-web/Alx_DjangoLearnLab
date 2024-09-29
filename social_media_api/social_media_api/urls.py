0f567924d76c172a2dd7dde03c7c489f294b64f8
abeekuhag <abee.hagan@gmail.com>
2024-09-29 23:23:29 +0000

    Implemented pagination and filtering

--- social_media_api/social_media_api/urls.py
+++ social_media_api/social_media_api/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('', lambda request: HttpResponseRedirect('/api/accounts/')),  # Redirect root to accounts
     path('admin/', admin.site.urls),
     path('api/accounts/', include('accounts.urls')),
+    path('api/', include('posts.urls')),
 ]
