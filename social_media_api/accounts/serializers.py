51219851a263e4bdf6fb0f10b726fa8b8e2d7624
abeekuhag <abee.hagan@gmail.com>
2024-09-29 21:51:43 +0000

    Updated accounts/serializers.py

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
+from django.contrib.auth.models import BaseUserManager
 from rest_framework.authtoken.models import Token
 
 User = get_user_model()
@@ -10,12 +11,26 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ['id', 'username', 'email', 'bio', 'profile_picture']
 
 class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True, style={'input_type': 'password'})
+    password_confirm = serializers.CharField(write_only=True, required=True, style={'input_type': 'password'})
+    
+    
     class Meta:
         model = User
         fields = ['username', 'password', 'email']
         extra_kwargs = {'password': {'write_only': True}}
 
+    def validate(self, data):
+        """
+        Check that the two password entries match.
+        """
+        if data['password'] != data['password_confirm']:
+            raise serializers.ValidationError("Passwords must match.")
+        return data
+
     def create(self, validated_data):
+        validated_data.pop('password_confirm')
+
         user = User.objects.create_user(
             username=validated_data['username'],
             password=validated_data['password'],
